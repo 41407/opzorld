b0345c06bacec4cb277200a091f938d14133dadc
41407 <jimi.welling@cs.helsinki.fi>
2015-04-17 22:52:38 +0300

    Beautify capture_Time

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,6 +4,11 @@ class Entry < ActiveRecord::Base
   has_attached_file :photo, :styles => { :original => "1000x1000>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 
+  def month_and_day
+    capture_time.strftime("%b %d").downcase
+  end
+
+private
   def parse_capture_time_from_filename
     update(capture_time: Date.parse(self.photo.original_filename))
   end
