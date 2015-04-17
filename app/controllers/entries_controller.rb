09ff491539cb24992ed9ec071b71b36839b31b09
41407 <jimi.welling@cs.helsinki.fi>
2015-04-17 22:12:18 +0300

    Entry -> new, show

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,38 @@
 class EntriesController < ApplicationController
   def show
+    @entry = Entry.find params[:id]
   end
 
   def index
+  end
+
+  def new
+    @entry = Entry.new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def create
+    @entry = Entry.new(entry_params)
+    respond_to do |format|
+      if @entry.save
+        format.html { redirect_to @entry }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:photo, :description)
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,4 +1,4 @@
 class Entry < ActiveRecord::Base
-  has_attached_file :avatar, :styles => { :original => "1000x1000>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :photo, :styles => { :original => "1000x1000>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
 end
