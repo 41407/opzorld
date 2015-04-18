6fc8debd2bfa3ab30528e682ac1207083e837667
41407 <jimi.welling@cs.helsinki.fi>
2015-04-18 15:11:11 +0300

    Fix wrong s3 credentials

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
   :storage => :s3,
   :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    :bucket => ENV['OPZ_S3_BUCKET_NAME'],
+    :access_key_id => ENV['OPZ_AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['OPZ_AWS_SECRET_ACCESS_KEY']
     }
   }
 
