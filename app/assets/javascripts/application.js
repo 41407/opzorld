50497efe67c63d4a959314d4eb9af2ac614e29d7
41407 <jimi.welling@cs.helsinki.fi>
2015-07-09 01:33:23 +0300

    Fix back body height

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ $(document).ready(function() {
   if ($('.pagination').length) {
     $(window).scroll(function() {
       var url = $('.pagination .next_page').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 150) {
         $('.pagination').text("Please Wait...");
         return $.getScript(url);
       }
